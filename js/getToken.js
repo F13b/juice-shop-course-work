8ba1161329d5d564f7e89afb9fcbc0ab3a0c80e4
F13b <74062671+F13b@users.noreply.github.com>
2022-05-23 23:35:48 +0300

    Some errors have been corrected in some view files, such as different or completely missing tab icons and similar flaws.

--- js/getToken.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const token = document.querySelector('.logo-activities');
-    localStorage.setItem('token', token.dataset.token);
-    token.dataset.token = '';
-});
